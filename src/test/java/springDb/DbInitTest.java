70340002cbd290b83a7a091094edc56934402354
jw <jw@Yoon-PC>
2017-11-28 09:50:17 +0900

    init db test

--- src/test/java/springDb/DbInitTest.java
+++ src/test/java/springDb/DbInitTest.java
@@ -1,14 +1,22 @@
 package springDb;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -18,17 +26,30 @@ import user.dao.UserDao;
 @ContextConfiguration(locations={"/springDb/application-context.xml",
 								"/springDb/context-datasource.xml"})
 public class DbInitTest {
-	//Logger logger = LoggerFactory.getLogger(DbInitTest.class);
+	Logger logger = LoggerFactory.getLogger(DbInitTest.class);
 	
 	@Resource(name="userDao")
 	private UserDao userDao;
 	
+	@Resource(name="dataSource")
+	private	DataSource dataSource;
+
+	@Before
+	public void setup() {
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(new ClassPathResource("/springDb/init.sql"));
+		populator.setContinueOnError(true);
+		DatabasePopulatorUtils.execute(populator , dataSource);
+	}
+	
+	@After
+	public void tearDown() {
+		//test 실행이 완료된 이후 정리 작업
+	}
+	
 	@Test
 	public void getUserlistTest() {
 		List<Map<String, String>> userList =  userDao.getUserList();
-		for(Map<String, String> map : userList)
-			System.out.println(map);
-			//logger.debug(map.toString());
+		assertEquals(3, userList.size());
 	}
-
 }
