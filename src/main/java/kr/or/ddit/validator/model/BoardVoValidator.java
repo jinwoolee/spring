b3422e703184ad374fd4725f7defb5534ec0b7cf
jw <jwlee1728@gmail.com>
2018-05-31 11:12:06 +0900

    boardVo validatorutils 주석 생성

--- src/main/java/kr/or/ddit/validator/model/BoardVoValidator.java
+++ src/main/java/kr/or/ddit/validator/model/BoardVoValidator.java
@@ -1,10 +1,14 @@
 package kr.or.ddit.validator.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 public class BoardVoValidator implements Validator {
+	Logger logger = LoggerFactory.getLogger(BoardVoValidator.class);
 
 	@Override
 	public boolean supports(Class<?> clazz) {
@@ -13,6 +17,7 @@ public class BoardVoValidator implements Validator {
 
 	@Override
 	public void validate(Object target, Errors errors) {
+		logger.debug("{}", "validate");
 		BoardVo boardVo = (BoardVo)target;
 		
 		if(boardVo.getReg_id() == null || boardVo.getReg_id().equals(""))
@@ -25,8 +30,14 @@ public class BoardVoValidator implements Validator {
 			errors.rejectValue("cont", "required");
 		
 		
+		//위 문장은 아래 문장과 동일
 //		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "reg_id", "required");
 //		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title", "required");
 //		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "cont", "required");
+
+		//errors에는 이미 target에 대한 정보가 설정 되어 있다
+//		BindingResult bindingResult = (BindingResult)errors;
+//		logger.debug("target : {}", bindingResult.getTarget().toString());
+		
 	}
 }
