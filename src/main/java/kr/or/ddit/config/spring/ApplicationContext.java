c51833f952258c9fe1812a89c6dc9dadbe425f22
jwlee <jwlee.lecture@gmail.com>
2019-03-22 12:30:53 +0900

    spring config 수정(xml->java config)

---
+++ src/main/java/kr/or/ddit/config/spring/ApplicationContext.java
@@ -0,0 +1,26 @@
+package kr.or.ddit.config.spring;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Configuration
+@ComponentScan(basePackages={"kr.or.ddit"}, useDefaultFilters=false, 
+	includeFilters={@Filter(type=FilterType.ANNOTATION, classes={Repository.class, Service.class})})
+public class ApplicationContext {
+	
+	@Bean
+	public MessageSource messageSource(){
+		ReloadableResourceBundleMessageSource messageSource = 
+				new ReloadableResourceBundleMessageSource();
+		messageSource.setBasenames("classpath:kr/or/ddit/config/msg/error",
+								   "classpath:kr/or/ddit/config/msg/msg");
+		return messageSource;
+	}
+}
---
+++ src/main/java/kr/or/ddit/config/spring/TransactionContext.java
@@ -0,0 +1,59 @@
+package kr.or.ddit.config.spring;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+@Import(value={DatasourceContext.class})
+@Configuration
+public class TransactionContext implements TransactionManagementConfigurer{
+	private Logger logger = LoggerFactory.getLogger(TransactionContext.class);
+	
+	@Resource(name="datasource")
+	private DataSource datasource;
+	
+	@Bean
+	public PlatformTransactionManager trasnsactionManager(){
+		logger.debug("trasnsactionManager : {}", datasource);
+		
+		DataSourceTransactionManager transactionManager =
+				new DataSourceTransactionManager();
+			transactionManager.setDataSource(datasource);
+		return transactionManager;
+	}
+
+	@Override
+	public PlatformTransactionManager annotationDrivenTransactionManager() {
+		return trasnsactionManager();
+	}
+	
+//	<bean id="transactionManager"
+//		 	class="org.springframework.jdbc.datasource.DataSourceTransactionManager">
+//		 	<property name="dataSource" ref="datasource"/>
+//		 </bean>
+//		 
+//		 <!--
+//		 	<tx:method rollback-for : ""/>
+//		 	기본설적으로 RuntimeExcpetion이 발생할 경우 자동 rollback처리  
+//		 	RuntimeException이 아닌 다른 예외 발생시 rollback 을 처리 하고 싶을 때 설정하는 옵션
+//		 
+//		  -->
+//		 <tx:advice id="txAdvice" transaction-manager="transactionManager">
+//		 	<tx:attributes>
+//		 		<tx:method name="*" propagation="REQUIRED"/>
+//		 	</tx:attributes>
+//		 </tx:advice>
+//		 
+//		 <aop:config>
+//		 	<aop:advisor advice-ref="txAdvice" 
+//		 				 pointcut="within(kr.or.ddit..service.*)"/>
+//		 </aop:config>
+}
