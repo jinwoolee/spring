36b8e167b951086b3800b4034a22691827d20dfd
jw <jwlee1728@gmail.com>
2019-06-20 13:32:54 +0900

    spring ioc bean life cycle  - ApplicationContextAware 구현     : sping ioc 객체를 인자로 받을수 있다  - <bean init-method="init">     : 개발자가 임의의 초기화 로직을 구성할수 있는 메소드를 스프링 빈 객체 생성시 적용

--- src/main/java/kr/or/ddit/ioc/placeholder/DbInfo.java
+++ src/main/java/kr/or/ddit/ioc/placeholder/DbInfo.java
@@ -1,10 +1,22 @@
 package kr.or.ddit.ioc.placeholder;
 
-public class DbInfo {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import kr.or.ddit.board.dao.BoardDao;
+import kr.or.ddit.board.dao.IboardDao;
+
+public class DbInfo implements ApplicationContextAware{
+	
 	private String driver;
 	private String url;
 	private String username;
 	private String password;
+	
+	private static final Logger logger = LoggerFactory.getLogger(DbInfo.class);
 	
 	public DbInfo(String driver, String url, String username, String password) {
 		this.driver = driver;
@@ -28,6 +40,23 @@ public class DbInfo {
 	public String getPassword() {
 		return password;
 	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		IboardDao boardDao = applicationContext.getBean("boardDao", BoardDao.class);
+		String msg = boardDao.sayHello();
+		logger.debug("msg : {}", msg);
+	}
+	
+	public void init() {
+		logger.debug("init-method");
+	}
 	
 	
 }
+
+
+
+
+
+
