ef5333da871e7b02b1a935fec26a6c62ff76160a
jw <jwlee1728@gmail.com>
2019-06-20 11:10:13 +0900

    spring conatiner scope test  - singleton  - prototype

--- src/main/java/kr/or/ddit/board/service/BoardService.java
+++ src/main/java/kr/or/ddit/board/service/BoardService.java
@@ -4,16 +4,22 @@ import kr.or.ddit.board.dao.IboardDao;
 
 public class BoardService implements IboardService{
 	
+	//property or filed
 	private IboardDao boardDao;
 	private String name;
+	
+	public BoardService() {
+	}
+	
+	public BoardService(IboardDao boardDao) {
+		this.boardDao = boardDao;
+	}
 
 	@Override
 	public String sayHello() {
 		return boardDao.sayHello();
 	}
 	
-
-
 	public void setBoardDao(IboardDao boardDao) {
 		this.boardDao = boardDao;
 	}
--- src/test/java/kr/or/ddit/ioc/SpringIocJunitTest.java
+++ src/test/java/kr/or/ddit/ioc/SpringIocJunitTest.java
@@ -11,14 +11,44 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import kr.or.ddit.board.dao.IboardDao;
 import kr.or.ddit.board.service.IboardService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:kr/or/ddit/ioc/application-ioc-test.xml")
 public class SpringIocJunitTest {
 	
+	//field 기준 boardService, boardService2 : spring boardService bean (scope=singleton)
+	//			     boardServiceConstructor : spring boardServiceConstructor bean (scope=singleton)
+	// 1. boardService, boardService2  :  같아야함
+	// 2. boardService, boardServiceConstructor : 같은 클래스에서 만들어진 객체이지만 spring singleton 개념에 따라 다른 객체
+	// 3. boardService2, boardServiceConstructor : 같은 클래스에서 만들어진 객체이지만 spring singleton 개념에 따라 다른 객체
+
+	//boardDao : spring boardDao(scope=singleton)
+	//boardDaoPrototype : spring boardDaoPrototype(scope=prototype)
+	//boardDaoPrototype2 : spring boardDaoPrototype(scope=prototype)
+	//1. boardDao boardDaoPrototype : spring bean id가 다르므로 다른객체
+	//2. boardDaoPrototype, boardDaoPrototype2 : 두객체는 같은 스프링 빈이지만
+	//																		scope가 prototype이므로 다른 객체 여야한다
+	@Resource(name="boardDao")
+	private IboardDao boardDao;
+	
+	@Resource(name="boardDaoPrototype")
+	private IboardDao boardDaoPrototype;
+	
+	@Resource(name="boardDaoPrototype")
+	private IboardDao boardDaoPrototype2;
+	
+	
 	@Resource(name="boardService")
 	private IboardService boardService;
+	
+	@Resource(name="boardService")
+	private IboardService boardService2;
+	
+	@Resource(name="boardServiceConstructor")
+	private IboardService boardServiceConsturctor;
+
 
 	/** 
 	 * Method   : springIocTest
@@ -37,7 +67,45 @@ public class SpringIocJunitTest {
 		assertNotNull(boardService);
 		assertEquals("boardDao sayHello", msg);
 	}
+	
+	/** 
+	 * Method   : springSingletonScopeTest
+	 * 작성자 : SEM
+	 * 변경이력 :  
+	 * Method 설명 : 스프링 컨테이너의 singleton scope 테스트 
+	 */
+	@Test
+	public void springSingletonScopeTest() {
+		/***Given***/
+		
+		/***When***/
 
+		/***Then***/
+		assertNotNull(boardService);
+		assertNotNull(boardService2);
+		assertNotNull(boardServiceConsturctor);
+		assertEquals(boardService, boardService2);
+		assertNotEquals(boardService, boardServiceConsturctor);
+		assertNotEquals(boardService2, boardServiceConsturctor);
+	}
+
+	@Test
+	public void springPrototypeScopeTest() {
+		/***Given***/
+		//1. boardDao boardDaoPrototype : spring bean id가 다르므로 다른객체
+		//2. boardDaoPrototype, boardDaoPrototype2 : 두객체는 같은 스프링 빈이지만
+		//																		scope가 prototype이므로 다른 객체 여야한다
+
+		/***When***/
+
+		/***Then***/
+		assertNotNull(boardDao);
+		assertNotNull(boardDaoPrototype);
+		assertNotNull(boardDaoPrototype2);
+		assertNotEquals(boardDao, boardDaoPrototype);
+		assertNotEquals(boardDaoPrototype, boardDaoPrototype2);
+
+	}
 }
 
 
