4948693323bf419d4b21b66ef3fa7c97c373ca34
jwlee <jwlee.lecture@gmail.com>
2019-09-16 15:50:08 +0900

    spring framework web.xml 설정 	. root conatiner (context load listener) 		.service, dao 	.child continer (dispatcher servlet) 		.controller

---
+++ src/main/java/kr/or/ddit/user/web/UserController.java
@@ -0,0 +1,40 @@
+package kr.or.ddit.user.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("user/")
+@Controller
+public class UserController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	
+	/**
+	* Method : userView
+	* 작성자 : SEM-PC
+	* 변경이력 :
+	* @return
+	* Method 설명 : 사용자 상세화면 요청
+	*/
+	//사용자가 localhost:8081/spring/user/view.do
+	@RequestMapping("view.do")
+	public String userView() {
+		logger.debug("userController.userView()");
+		
+		return "user/view";
+		
+		// prefix + viewName + suffix
+		// WEB-INF/views/user/view.jsp
+	}
+
+
+}
+
+
+
+
+
+
+
