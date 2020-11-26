07477e3de8d368d030c733d2a599baae2a2fb0d6
DESKTOP-BHBJQVH\jw-sem <jwlee.lecture@gmail.com>
2020-11-26 11:22:06 +0900

    SecurityContextHolder를 이용한 인증 설정

--- src/main/java/kr/or/ddit/main/web/MainController.java
+++ src/main/java/kr/or/ddit/main/web/MainController.java
@@ -1,21 +1,64 @@
 package kr.or.ddit.main.web;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.or.ddit.user.model.UserVo;
+import kr.or.ddit.user.repository.UserDaoI;
+import kr.or.ddit.user.security.model.UserDetail;
+
 @Controller
 public class MainController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
+	@Resource(name="userDao")
+	private UserDaoI userDao;
+	
+	@RequestMapping("/backDoor")
+	public String backDoor() {
+		
+		//사용자 정보 조회
+		UserVo userVo = userDao.getUser("brown");
+		
+		//사용자 권한 조회
+		List<String> roleList = userDao.getRoles("brown");
+				
+		List<GrantedAuthority> roles = new ArrayList<GrantedAuthority>();
+		for(String role : roleList) {
+			roles.add(new SimpleGrantedAuthority(role));
+		}
+				
+		UserDetail userDetail = new UserDetail(userVo, roles);
+				
+		//생성자에서 authenticated 속성을 true로 변경
+		Authentication authentication = new UsernamePasswordAuthenticationToken(userDetail, "12345", roles);
+		
+		logger.debug("authentication.isAuthenticated() : {} " , authentication.isAuthenticated());
+		
+		//인증정보 설정
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
+		logger.debug("authentication.isAuthenticated() : {} " , authentication.isAuthenticated());
+		
+		return "main";
+	}
 	
 	//principal : 인증 정보를 담고 있다
 	@RequestMapping("/")
@@ -45,8 +88,8 @@ public class MainController {
 	}
 	
 	@RequestMapping("/main")
-	public String main() {
-		logger.debug("MainController.main()");		
+	public String main(Authentication authentication) {
+		logger.debug("MainController.main(), {}", authentication.isAuthenticated());		
 		return "main";
 	}
 	
--- src/main/webapp/WEB-INF/views/main.jsp
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>    
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>    
 <!DOCTYPE html> 
 <html>
 <head>
@@ -8,7 +9,8 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	<sec:authentication property="principal"/>
+	
 	main.jsp
 	<a href="/logout" >[logout]</a>
 </body>
