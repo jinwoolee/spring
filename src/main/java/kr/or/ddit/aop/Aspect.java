12a79927f346df37aaeea282dec477e5fdbcc2f1
jwlee <jwlee.lecture@gmail.com>
2021-02-05 17:03:23 +0900

    spring AOP  . before  . around  . 프로파일링  . @Aspect

---
+++ src/main/java/kr/or/ddit/aop/Aspect.java
@@ -0,0 +1,62 @@
+package kr.or.ddit.aop;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@org.aspectj.lang.annotation.Aspect
+public class Aspect {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Aspect.class);
+	
+	@Pointcut("execution(* kr.or.ddit..service.*.*(..))")
+	public void dummy() {}
+	
+	//특정 메소드가 실행되기전에 실행 되어야할 공통의 관심사항
+	@Before("dummy()")
+	public void beforeMethod(JoinPoint joinPoint) {
+		logger.debug("Aspect.beforeMehtod");
+	}
+	
+	//around : 특정 메소드 실행 전후
+	//    메소드 실행전 - 공통 관심사항
+	//    메소드의 원래 로직
+	//    메소드 실행후 - 공통 관심사항
+	
+	@Around("dummy()")
+	public Object aroundMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+		
+		//메소드 본 로직 실행전
+		long startTime = System.nanoTime();
+		
+		String className = joinPoint.getTarget().getClass().getSimpleName();
+		String methodName = joinPoint.getSignature().getName();
+		
+		Object ret = joinPoint.proceed(joinPoint.getArgs());
+		
+		//메소드 본 로직 실행후
+		long endTime = System.nanoTime();
+		
+		logger.debug(" {} {} : duration : {}", className, methodName, endTime-startTime);
+		
+		return ret;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
